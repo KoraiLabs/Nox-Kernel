ee06b688139cdaec32a7b62740406081ba4ab39f
KoraiLabs <zric.ote@gmail.com>
2025-01-26 10:10:20 +0100

    Delete out directory

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 8 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#8 SMP PREEMPT Sat Jan 18 08:04:42 CET 2025"
-#define LINUX_COMPILE_BY "deck"
-#define LINUX_COMPILE_HOST "steamdeck"
-#define LINUX_COMPILER "Android (7284624, based on r416183b) clang version 12.0.5 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee), GNU ld (binutils-2.27-bd24d23f) 2.27.0.20170315"
